809cdb9b92dc2de4eaa5ba53408456f967a1fcd9
Aimar Aareleid <aareleid@worth.systems>
2020-12-07 18:01:03 +0100

    update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,25 +3,8 @@ import React from "react";
 
 import { Form, Button } from "antd";
 import { LoginWrapper } from "./LoginStyles";
-// import { Link } from "react-router-dom";
-// import { UserContext } from "../context/UserContext";
-
-// const LoginButton = (props) => (
-//   <Link to={`${process.env.REACT_APP_API_URL}/connect/google`}></Link>
-//   // <a href={`${process.env.API_URL}/connect/google`}>
-//   //   <button style={{ width: "150px" }}>Connect to {props.providerName}</button>
-//   // </a>
-// );
 
 export default function Login() {
-  // const history = useHistory();
-
-  // const [isLogged, setIsLogged] = useState(!!localStorage.getItem("jwt"));
-
-  // if (isLogged) {
-  //   history.push("/");
-  // }
-
   return (
     <LoginWrapper>
       <img
